193d85790f9e6b610553e1977a8e4a462b640aea
Eli <Eli.Ateljevich@water.ca.gov>
2025-01-16 19:58:04 -0800

    Added current experiments to repo.

---
+++ casanntra/tide_transforms.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def difference(x):
+    return x.diff().bfill()
+
+def append_tidal_pca_cols(df):
+    data_normalized = df[['sf_tidal_filter', 'sf_tidal_energy','case']] 
+    data_normalized["sf_tidal_filter"] -= data_normalized.groupby("case")["sf_tidal_filter"].transform("mean")
+    data_normalized["sf_tidal_filter"] /= data_normalized.sf_tidal_filter.std()
+    data_normalized["sf_tidal_energy"] -=  data_normalized.sf_tidal_energy.mean()  # Note global for energy, group for filter
+    data_normalized["sf_tidal_energy"] /= data_normalized.sf_tidal_energy.std()
+    data_normalized = data_normalized.drop("case",axis=1)
+
+
+    # Perform PCA
+    pca = PCA(n_components=2)
+    pca_loadings = pca.fit_transform(data_normalized)
+    print(data_normalized.columns)
+    pca_df = pd.DataFrame(index=data_normalized.index,
+                        data={'tidal_pc1': pca_loadings[:, 0],
+                        'tidal_pc2': pca_loadings[:, 1]})
+    df_out = pd.concat([df,pca_df],axis=1)
+    return df_out
