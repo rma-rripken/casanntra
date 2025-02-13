df2dd5a7e8e2b246355dd9d455ffe0c81fa9aec7
Eli <Eli.Ateljevich@water.ca.gov>
2025-02-13 11:31:01 -0800

    Documented the example.

--- example/transfer_example.py
+++ example/transfer_example.py
@@ -2,10 +2,19 @@ from casanntra.staged_learning import process_config
 
 
 def main():
+    """ Ultimately we may launch this using argparse, but it is hard wired right now. 
+        The first argument, configfile, gives the configuration steps for DSM2 base, DSM2->SCHISM
+        and SCHISM,base -> SCHISM,suisun.
+
+        My visualization of the cross validation output is vis_output.py. 
+        It is in much more rudimentary state.
+    """
     configfile = "transfer_config.yml"
-    process_config(configfile, ["dsm2_base", "dsm2.schism", "base.suisun"])
-    # process_config(configfile, ["dsm2.schism", "base.suisun"])
-    # process_config(configfile, ["base.suisun"])
+
+    # This selects the steps you want to run
+    #process_config(configfile, ["dsm2_base", "dsm2.schism", "base.suisun"])
+    process_config(configfile, ["dsm2.schism", "base.suisun"])
+    #process_config(configfile, ["base.suisun"])
     # process_config(configfile, ["dsm2_base"])
 
 
