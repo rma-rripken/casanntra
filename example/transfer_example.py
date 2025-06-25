0267b2a3997dc09df6f8bc0f960031b14ad7d3e6
canruso <canruso@berkeley.edu>
2025-06-25 13:05:43 -0700

    commit local files to prep for pull

--- example/transfer_example.py
+++ example/transfer_example.py
@@ -2,20 +2,20 @@ from casanntra.staged_learning import process_config
 
 
 def main():
-    """ Ultimately we may launch this using argparse, but it is hard wired right now. 
-        The first argument, configfile, gives the configuration steps for DSM2 base, DSM2->SCHISM
-        and SCHISM,base -> SCHISM,suisun.
+    """Ultimately we may launch this using argparse, but it is hard wired right now.
+    The first argument, configfile, gives the configuration steps for DSM2 base, DSM2->SCHISM
+    and SCHISM,base -> SCHISM,suisun.
 
-        My visualization of the cross validation output is vis_output.py. 
-        It is in much more rudimentary state.
+    My visualization of the cross validation output is vis_output.py.
+    It is in much more rudimentary state.
     """
-    configfile = "transfer_config.yml"
+    configfile = "transfer_config_v2.yml"
 
     # This selects the steps you want to run
-    #process_config(configfile, ["dsm2_base", "dsm2.schism", "base.suisun"])
-    #process_config(configfile, ["dsm2.schism", "base.suisun"])
-    #process_config(configfile, ["dsm2.schism"])
-    process_config(configfile, ["base.suisun"])
+    process_config(configfile, ["dsm2_base", "dsm2.schism", "base.suisun"])
+    # process_config(configfile, ["dsm2.schism", "base.suisun"])
+    # process_config(configfile, ["dsm2.schism"])
+    # process_config(configfile, ["base.suisun"])
     # process_config(configfile, ["dsm2_base"])
 
 
