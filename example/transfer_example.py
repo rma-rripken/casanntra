4a2a2ad7c0b859e272a19b68256634e2e6c11214
ryan <89810919+rma-rripken@users.noreply.github.com>
2025-07-23 14:53:05 -0700

    Full suite of steps

--- example/transfer_example.py
+++ example/transfer_example.py
@@ -1,5 +1,8 @@
 from casanntra.staged_learning import process_config
-
+import multiprocessing as mp
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 def main():
     """Ultimately we may launch this using argparse, but it is hard wired right now.
@@ -12,12 +15,11 @@ def main():
     configfile = "transfer_config_v2.yml"
 
     # This selects the steps you want to run
-    process_config(configfile, ["dsm2_base", "dsm2.schism", "base.suisun"])
-    # process_config(configfile, ["dsm2.schism", "base.suisun"])
-    # process_config(configfile, ["dsm2.schism"])
-    # process_config(configfile, ["base.suisun"])
-    # process_config(configfile, ["dsm2_base"])
+    process_config(configfile, ["dsm2_base"])
+    process_config(configfile, [ "dsm2.schism", "base.suisun","base.slr", "base.ft", "base.cache"])
+    process_config(configfile, ["dsm2.rma", "rma_base.suisun", "rma_base.ft", "rma_base.cache"])
 
 
 if __name__ == "__main__":
+    mp.set_start_method("spawn", force=True)
     main()
