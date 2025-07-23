120fd9af9b6af0bc293e9fb83653dd33c7bba677
ryan <89810919+rma-rripken@users.noreply.github.com>
2025-07-23 14:55:49 -0700

    Full suite of steps

--- example/x2_transfer_example.py
+++ example/x2_transfer_example.py
@@ -10,14 +10,12 @@ import multiprocessing as mp
 from casanntra.staged_learning import process_config
 
 
-
 def main():
     configfile = "x2_transfer_config.yml"
 
-    process_config(configfile, ["x2_dsm2_base", "x2_dsm2.schism", "x2_base.suisun", "x2_dsm2.rma", "x2_rma_base.suisun", "x2_rma_base.cache", "x2_rma_base.ft"])
-    # "x2_dsm2.rma", "x2_rma_base.ft", "x2_rma_base.cache"])
-    #process_config(configfile, ["x2_dsm2.schism"])
-    #process_config(configfile, ["x2_base.suisun"])
+    process_config(configfile, ["x2_dsm2_base"])
+    process_config(configfile, ["x2_dsm2.schism", "x2_base.suisun","x2_base.slr","x2_base.cache" ])
+    process_config(configfile, [ "x2_dsm2.rma", "x2_rma_base.suisun", "x2_rma_base.cache", "x2_rma_base.ft","x2_base.slr"])
 
 if __name__ == "__main__":
     mp.set_start_method("spawn", force=True)
